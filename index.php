358a1888faf7a2469583032fd8dc5f11563c9778
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-09-12 17:23:44 -0700

    Added one more multidimensional array chocolate

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ $offers=[
   ['name' => 'toffee' , 'price' => 5, 'stock'=>120,],
   ['name' => 'Mints' , 'price' => 3, 'stock'=>66,],
   ['name' => 'Fudge' , 'price' => 4, 'stock'=>97,],
+  ['name' => 'chocolate' , 'price' => 2, 'stock'=>83,],
 ]
 ;
 
@@ -15,11 +16,15 @@ $offers=[
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Nutrition (per 100g)</h2>
-   <p>Fat: <?php echo $nutrition['fat']; ?>%</p>
-   <p>Sugar: <?php echo $nutrition['sugar']; ?>%</p>
-   <p>Salt: <?php echo $nutrition['salt']; ?>%</p>
-   <p>Fiber: <?php echo $nutrition['fiber']; ?>%</p>
-   <p>protein: <?php echo $nutrition['protien']; ?>%</p>
+    <h2>Offers</h2>
+   <p> <?php echo $offers[0] ['name']; ?>
+   <?php echo $offers[0] ['price']; ?>
+  </p>
+  <p> <?php echo $offers[1] ['name']; ?>
+   <?php echo $offers[1] ['price']; ?>
+  </p>
+  <p> <?php echo $offers[2] ['name']; ?>
+   <?php echo $offers[2] ['price']; ?>
+  </p>
   </body>
 </html>
