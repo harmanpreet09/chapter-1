317c8bfed494352828ed371560395f9f077827c2
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-09-12 18:12:06 -0700

    Logical Operator with swap value

--- index.php
+++ index.php
@@ -1,21 +1,22 @@
 <?php 
 $item    = 'Chocolate';
-$stock   = 5;
-$wanted  = 8;
-$can_buy = ($wanted <= $stock);
+$stock   = 3;
+$wanted  = 5;
+$deliver = true;
+$can_buy = (($wanted <= $stock) && ($deliver == true));
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Comparison Operators</title>
+    <title>Logical Operators</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Shopping Cart</h2>
+    <h1>Shopping Cart</h1>
     <p>Item:    <?= $item ?></p>
     <p>Stock:   <?= $stock ?></p>
-    <p>Wanted:  <?= $wanted ?></p>
+    <p>Wanted: <?= $wanted ?></p>
     <p>Can buy: <?= $can_buy ?></p>
   </body>
 </html>
