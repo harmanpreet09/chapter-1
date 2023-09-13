1cad6ef76d6a00c990c6ee4d4c4a7b4c6cf639c6
harmanpreet09 <122574728+harmanpreet09@users.noreply.github.com>
2023-09-12 17:38:38 -0700

    Added my name to the variable and add one more favorite chocolate type in the array

--- index.php
+++ index.php
@@ -1,29 +1,21 @@
 <?php 
 $name= 'Harman Preet Kaur';
-$favorites=['Chocolate' , 'Toffee' , 'Fudge']
+$favorites=['Chocolate' , 'Toffee' , 'Fudge' , 'Muffins']
 ;
 
 ?>
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Variables</title>
+    <title>Echo Shorthand</title>
     <link rel="stylesheet" href="css/styles.css">
   </head>
   <body>
     <h1>The Candy Store</h1>
-    <h2>Offers</h2>
-   <p> <?php echo $offers[0] ['name']; ?>
-   <?php echo $offers[0] ['price']; ?>
-  </p>
-  <p> <?php echo $offers[1] ['name']; ?>
-   <?php echo $offers[1] ['price']; ?>
-  </p>
-  <p> <?php echo $offers[2] ['name']; ?>
-   <?php echo $offers[2] ['price']; ?>
-  </p>
-  <p> <?php echo $offers[3] ['name']; ?>
-   <?php echo $offers[3] ['price']; ?>
-  </p>
+    <h2>Welcome <? $name ?></h2>
+    <p>Your favorite type of candy is:
+      <?= $favorites[0] ?>
+    </p>
+   
   </body>
 </html>
